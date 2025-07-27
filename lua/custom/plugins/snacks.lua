c564b24c2fde1cd046a16690b5aca4fc60f315dd
Dwcks <corclan19@gmail.com>
2025-07-28 01:17:10 +0700

    fix dashboard

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -30,7 +30,7 @@ return {
           -- When using a function, the `items` argument are the default keymaps.
           ---@type snacks.dashboard.Item[]
           keys = {
-            -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
             { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
             -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
@@ -41,12 +41,12 @@ return {
               action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
             },
             { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
-            {
-              icon = ' ',
-              key = 'x',
-              desc = 'Lazy Extras',
-              action = ':LazyExtras',
-            },
+            -- {
+            --   icon = ' ',
+            --   key = 'x',
+            --   desc = 'Lazy Extras',
+            --   action = ':LazyExtras',
+            -- },
             { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
             { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
           },

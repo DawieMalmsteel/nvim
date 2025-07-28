88df4ebbb96b9e5c017b4dc7951508f5b130d45f
Dwcks <corclan19@gmail.com>
2025-07-29 00:16:04 +0700

    deleted unneed options

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -29,19 +29,6 @@ return {
     'CopilotC-Nvim/CopilotChat.nvim',
     branch = 'main',
     cmd = 'CopilotChat',
-    opts = function()
-      local user = vim.env.USER or 'User'
-      user = user:sub(1, 1):upper() .. user:sub(2)
-
-      return {
-        auto_insert_mode = true,
-        question_header = '  ' .. user .. ' ',
-        answer_header = '  Copilot ',
-        window = {
-          width = 0.4,
-        },
-      }
-    end,
     keys = {
       { '<c-s>', '<CR>', ft = 'copilot-chat', desc = 'Submit Prompt', remap = true },
       {

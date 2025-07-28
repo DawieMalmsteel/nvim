c0104d61c037d89d304cf87811d12a3f10cf8c2d
Dwcks <corclan19@gmail.com>
2025-07-29 03:10:29 +0700

    using mini.snippets instead of LuaSnip

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -16,9 +16,24 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
     require('mini.tabline').setup()
-    require('mini.notify').setup()
-    require('mini.sessions').setup()
-    -- TODO: add keybindings for sessions
+    require('mini.notify').setup() -- TODO: add keybindings for notifications
+
+    local mini_snippets = require 'mini.snippets'
+    mini_snippets.setup {
+      snippets = { mini_snippets.gen_loader.from_lang() },
+      mappings = {
+        -- Expand snippet at cursor position. Created globally in Insert mode.
+        expand = '<C-a>',
+
+        -- Interact with default `expand.insert` session.
+        -- Created for the duration of active session(s)
+        jump_next = '<C-l>',
+        jump_prev = '<C-h>',
+        stop = '<C-c>',
+      },
+    }
+
+    require('mini.sessions').setup() -- TODO: add keybindings for sessions
 
     require('mini.files').setup {
       windows = {

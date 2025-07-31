5fe9211058215bd77d963c9bf71f43314463f94d
Dwcks <corclan19@gmail.com>
2025-08-01 00:34:14 +0700

    added color support for my nvim

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -19,6 +19,44 @@ return { -- Collection of various small independent plugins/modules
     require('mini.surround').setup()
     require('mini.tabline').setup()
     require('mini.indentscope').setup()
+    require('mini.hipatterns').setup {
+      tailwind = {
+        enabled = true,
+        ft = {
+          'astro',
+          'css',
+          'heex',
+          'html',
+          'html-eex',
+          'javascript',
+          'javascriptreact',
+          'rust',
+          'svelte',
+          'typescript',
+          'typescriptreact',
+          'vue',
+        },
+        -- full: the whole css class will be highlighted
+        -- compact: only the color will be highlighted
+        style = 'full',
+      },
+      highlighters = {
+        hex_color = require('mini.hipatterns').gen_highlighter.hex_color { priority = 2000 },
+        shorthand = {
+          pattern = '()#%x%x%x()%f[^%x%w]',
+          group = function(_, _, data)
+            ---@type string
+            local match = data.full_match
+            local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
+            local hex_color = '#' .. r .. r .. g .. g .. b .. b
+
+            return MiniHipatterns.compute_hex_color_group(hex_color, 'bg')
+          end,
+          extmark_opts = { priority = 2000 },
+        },
+      },
+    }
+
     local mini_map = require 'mini.map'
     mini_map.setup {
       symbols = {

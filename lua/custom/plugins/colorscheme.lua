ec3b0928bf6061ca3f98c7060eed09456a5e9edb
Dwcks <corclan19@gmail.com>
2025-07-31 15:41:08 +0700

    fix colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,4 +1,10 @@
 return {
+  {
+    'AlexvZyl/nordic.nvim',
+    config = function()
+      vim.cmd.colorscheme 'nordic'
+    end,
+  },
   -- {
   --   'navarasu/onedark.nvim',
   --   opts = {
@@ -9,48 +15,35 @@ return {
   --   },
   -- },
   -- {
-  --   'folke/tokyonight.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --   },
-  -- },
-  -- {
-  --   'AlexvZyl/nordic.nvim',
-  -- },
-  -- {
   --   'glepnir/zephyr-nvim',
   --   requires = { 'nvim-treesitter/nvim-treesitter', opt = true },
   -- },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1001,
-    opts = {
-      transparent_background = false,
-    },
-  },
-  { 'sainnhe/gruvbox-material' },
-  { 'rebelot/kanagawa.nvim' },
-
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'catppuccin'
-      vim.cmd.colorscheme 'gruvbox-material'
-      -- vim.cmd.colorscheme 'kanagawa'
-    end,
-  },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1001,
+  --   opts = {
+  --     transparent_background = false,
+  --   },
+  -- },
+  -- { 'sainnhe/gruvbox-material' },
+  -- { 'rebelot/kanagawa.nvim' },
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     ---@diagnostic disable-next-line: missing-fields
+  --     require('tokyonight').setup {
+  --       styles = {
+  --         comments = { italic = false }, -- Disable italics in comments
+  --       },
+  --     }
+  --
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     -- vim.cmd.colorscheme 'catppuccin'
+  --     -- vim.cmd.colorscheme 'kanagawa'
+  --   end,
+  -- },
 }

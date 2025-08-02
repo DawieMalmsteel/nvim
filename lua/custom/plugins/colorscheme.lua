10ae5fc75d768b33a18fba7d703097e726adb1a6
Dwcks <corclan19@gmail.com>
2025-08-02 23:50:04 +0700

    changed colorscheme and added new colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -10,7 +10,29 @@ return {
       --     -- float = true,
       --   },
       -- }
-      vim.cmd.colorscheme 'nordic'
+      -- vim.cmd.colorscheme 'nordic'
+    end,
+  },
+
+  {
+    'V4N1LLA-1CE/xcodedark.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('xcodedark').setup {
+        transparent = true,
+        integrations = {
+          telescope = true,
+          nvim_tree = true,
+          gitsigns = true,
+          bufferline = true,
+          incline = true,
+          lazygit = true,
+          which_key = true,
+          notify = true,
+        },
+      }
+      vim.cmd.colorscheme 'xcodedark'
     end,
   },
   -- {

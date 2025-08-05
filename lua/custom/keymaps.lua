210aeac0ef24cf42bd6d7ce77f8ad84d3510455f
Dwcks <corclan19@gmail.com>
2025-08-05 22:24:28 +0700

    update desc for leader tab and added harpoon to my nvim

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -345,9 +345,6 @@ end, { desc = 'Load Last Session (Current Project)' })
 map('n', '<leader>usc', function()
   vim.cmd 'mksession'
 end, { desc = 'Create Session (:mksession)' })
+
 -- Edit file
-map('n', '<leader><Tab>', ':e<Space>', { desc = 'Edit file' })
--- Lazygit test
--- map('n', '<leader>gm', function()
---   vim.cmd 'Floaterminal lazygit'
--- end, { desc = 'Open LazyGit (Floaterminal)' })
+map('n', '<leader><Tab>', ':e<Space>', { desc = '+New file' })

6481d3c600d44cd03bef50f757d4fcf66cdd29dd
Dwcks <corclan19@gmail.com>
2025-08-16 01:39:26 +0700

    update for termux

--- init.lua
+++ init.lua
@@ -336,23 +336,6 @@ require('lazy').setup({
         vtsls = {},
 
         csharp_ls = {},
-
-        jdtls = {},
-
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
       }
 
       for server_name, server_config in pairs(servers) do

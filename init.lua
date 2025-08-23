2b3d7c7523c8e382fae5d6b0837f05ba0af7f75e
Dwcks <corclan19@gmail.com>
2025-08-23 13:21:55 +0700

    remove mini-snippets and use luasnip instead

--- init.lua
+++ init.lua
@@ -421,7 +421,29 @@ require('lazy').setup({
         'giuxtaposition/blink-cmp-copilot',
       },
       {
-        'rafamadriz/friendly-snippets',
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+        opts = {},
       },
       'folke/lazydev.nvim',
     },
@@ -442,7 +464,7 @@ require('lazy').setup({
         ghost_text = { enabled = vim.g.ai_cmp },
       },
 
-      snippets = { preset = 'mini_snippets' },
+      snippets = { preset = 'luasnip' }, --mini_snippets
 
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'copilot' },

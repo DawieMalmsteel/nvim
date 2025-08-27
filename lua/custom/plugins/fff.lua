d3ea3d3542856bb5552e3667dec72f2eb40137ed
Dwcks <corclan19@gmail.com>
2025-08-28 02:44:32 +0700

    added fff

--- lua/custom/plugins/fff.lua
+++ lua/custom/plugins/fff.lua
@@ -1,16 +1,24 @@
 return {
-  -- 'dmtrKovalenko/fff.nvim',
+  'dmtrKovalenko/fff.nvim',
   -- build = 'cargo build --release',
-  -- opts = {
-  --   -- pass here all the options
-  -- },
-  -- keys = {
-  --   {
-  --     '<leader>F', -- try it if you didn't it is a banger keybinding for a picker
-  --     function()
-  --       require('fff').toggle()
-  --     end,
-  --     desc = 'Toggle FFF',
-  --   },
-  -- },
+  -- or if you are using nixos
+  -- build = "nix run .#release",
+  opts = { -- (optional)
+    debug = {
+      enabled = true, -- we expect your collaboration at least during the beta
+      show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+    },
+  },
+  -- No need to lazy-load with lazy.nvim.
+  -- This plugin initializes itself lazily.
+  lazy = false,
+  keys = {
+    {
+      '<leader><CR>', -- try it if you didn't it is a banger keybinding for a picker
+      function()
+        require('fff').find_files()
+      end,
+      desc = 'FFFind files',
+    },
+  },
 }

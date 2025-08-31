fffd4d50ce0fc388eed05e9029fcb0c910a5cd4f
Dwcks <corclan19@gmail.com>
2025-08-31 21:39:00 +0700

    added mini bufremove

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -136,6 +136,7 @@ return { -- Collection of various small independent plugins/modules
     require('mini.visits').setup()
     require('mini.icons').setup()
     require('mini.git').setup()
+    require('mini.bufremove').setup()
     require('mini.statusline').setup()
 
     -- require('mini.clue').setup()

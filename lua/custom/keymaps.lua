ad701e4316c39288b9bfd36162f068501c81abc0
Dwcks <corclan19@gmail.com>
2025-08-31 21:39:13 +0700

    added keymaps for bufremove

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -553,3 +553,21 @@ end, { silent = false })
 
 map('n', "y'", "yi'", { noremap = true })
 map('n', 'y"', 'yi"', { noremap = true })
+
+-- Close Hidden Buffers
+map('n', '<leader>bh', function()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.fn.buflisted(buf) == 1 and vim.fn.bufwinnr(buf) == -1 then
+      require('mini.bufremove').delete(buf, false)
+    end
+  end
+end, { desc = 'Close Hidden Buffers' })
+
+-- Close Nameless Buffers
+map('n', '<leader>bu', function()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.fn.buflisted(buf) == 1 and vim.api.nvim_buf_get_name(buf) == '' then
+      require('mini.bufremove').delete(buf, false)
+    end
+  end
+end, { desc = 'Close Nameless Buffers' })

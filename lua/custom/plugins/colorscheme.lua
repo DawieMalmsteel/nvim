a929c574842e0369b96bff7b27830e4008ea94eb
Dwcks <corclan19@gmail.com>
2025-09-02 19:26:24 +0700

    bring zenbones back

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -14,6 +14,21 @@ return {
     },
   },
   {
+    'zenbones-theme/zenbones.nvim',
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = 'rktjmp/lush.nvim',
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    config = function()
+      vim.g.zenboes_compat = 1
+      vim.g.zenbones_darken_comments = 45
+      vim.cmd.colorscheme 'zenbones'
+    end,
+  },
+  {
     'AlexvZyl/nordic.nvim',
     config = function()
       require('nordic').setup {
@@ -25,7 +40,7 @@ return {
 
       -- vim.cmd.colorscheme 'monokai-nightasty'
       -- vim.opt.background = 'light'
-      vim.cmd.colorscheme 'miniautumn'
+      -- vim.cmd.colorscheme 'miniautumn'
 
       vim.cmd [[
           highlight TabLine guibg=NONE guifg=#abb2bf

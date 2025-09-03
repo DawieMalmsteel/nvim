6081aa8fcded66bede4256ff8c750a2f5757c700
Dwcks <corclan19@gmail.com>
2025-09-03 20:12:38 +0700

    added sql

--- init.lua
+++ init.lua
@@ -597,7 +597,7 @@ require('lazy').setup({
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'ocaml', 'c_sharp' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'ocaml', 'c_sharp', 'sql' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {

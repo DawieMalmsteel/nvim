d7aa731807486835a4f50cdd5f9b01700f4884ef
Dwcks <corclan19@gmail.com>
2025-09-03 19:43:07 +0700

    change back to mini snippets

--- init.lua
+++ init.lua
@@ -503,32 +503,7 @@ require('lazy').setup({
       {
         'giuxtaposition/blink-cmp-copilot',
       },
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-              require('luasnip.loaders.from_vscode').lazy_load { paths = { vim.fn.stdpath 'config' .. '/snippets' } }
-            end,
-          },
-        },
-        opts = {},
-      },
+      { 'rafamadriz/friendly-snippets' },
       'folke/lazydev.nvim',
     },
     --- @module 'blink.cmp'
@@ -548,7 +523,7 @@ require('lazy').setup({
         ghost_text = { enabled = vim.g.ai_cmp },
       },
 
-      snippets = { preset = 'luasnip' }, --mini_snippets
+      snippets = { preset = 'mini_snippets' },
 
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'copilot' },
--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -256,20 +256,20 @@ return { -- Collection of various small independent plugins/modules
     }
 
     -- Mini Snippets
-    -- local mini_snippets = require 'mini.snippets'
-    -- mini_snippets.setup {
-    --   snippets = { mini_snippets.gen_loader.from_lang() },
-    --   mappings = {
-    --     -- Expand snippet at cursor position. Created globally in Insert mode.
-    --     expand = '<C-a>',
-    --
-    --     -- Interact with default `expand.insert` session.
-    --     -- Created for the duration of active session(s)
-    --     jump_next = '<C-l>',
-    --     jump_prev = '<C-h>',
-    --     stop = '<C-c>',
-    --   },
-    -- }
+    local mini_snippets = require 'mini.snippets'
+    mini_snippets.setup {
+      snippets = { mini_snippets.gen_loader.from_lang() },
+      mappings = {
+        -- Expand snippet at cursor position. Created globally in Insert mode.
+        expand = '<C-a>',
+
+        -- Interact with default `expand.insert` session.
+        -- Created for the duration of active session(s)
+        jump_next = '<C-l>',
+        jump_prev = '<C-h>',
+        stop = '<C-c>',
+      },
+    }
 
     -- require('mini.sessions').setup()
 

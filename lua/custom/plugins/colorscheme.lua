275f38946139cb2b6c9f8c1cb5f041fad59ebdbc
Dwcks <corclan19@gmail.com>
2025-09-04 12:44:23 +0700

    update highlight color

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -56,55 +56,56 @@ return {
       -- vim.cmd.colorscheme 'randomhue'
 
       vim.cmd [[
-          highlight TabLine guibg=NONE guifg=#abb2bf
-          highlight TabLineSel guibg=#61afef guifg=#282c34
-          highlight TabLineFill guibg=NONE guifg=#5c6370
+           highlight TabLine guibg=NONE guifg=#7e8294
+           highlight TabLineSel guibg=#7aa2f7 guifg=#1a1b26
+           highlight TabLineFill guibg=NONE guifg=#3b4261
 
-          highlight MiniTablineCurrent guibg=NONE guifg=#61afef
-          highlight MiniTablineVisible guibg=NONE guifg=#abb2bf
-          highlight MiniTablineHidden guibg=NONE guifg=#5c6370
-          highlight MiniTablineModifiedCurrent guibg=NONE guifg=#e5c07b
-          highlight MiniTablineModifiedVisible guibg=NONE guifg=#e06c75
-          highlight MiniTablineModifiedHidden guibg=NONE guifg=#e5c07b
-          highlight MiniTablineTabpagesection guibg=NONE guifg=#98c379
-          highlight MiniTablineFill guibg=NONE guifg=#5c6370
-          highlight MiniTablineTrunc guibg=NONE guifg=#5c6370
-          highlight MiniStarterItemBullet guibg=NONE guifg=#61afef
-          highlight MiniHipatterns_abb2bf_bg guibg=NONE guifg=#abb2bf
-          highlight MiniHipatterns_61afef_bg guibg=NONE guifg=#61afef
+           highlight MiniTablineCurrent guibg=NONE guifg=#7aa2f7
+           highlight MiniTablineVisible guibg=NONE guifg=#7e8294
+           highlight MiniTablineHidden guibg=NONE guifg=#3b4261
 
-          highlight StatusLine guibg=NONE
-          highlight StatusLineNC guibg=NONE
-          highlight StatusLineTerm guibg=NONE
-          highlight StatusLineTermNC guibg=NONE
+           highlight MiniTablineModifiedCurrent guibg=NONE guifg=#e0af68
+           highlight MiniTablineModifiedVisible guibg=NONE guifg=#f7768e
+           highlight MiniTablineModifiedHidden guibg=NONE guifg=#e0af68
+           highlight MiniTablineTabpagesection guibg=NONE guifg=#9ece6a
+           highlight MiniTablineFill guibg=NONE guifg=#3b4261
+           highlight MiniTablineTrunc guibg=NONE guifg=#3b4261
+           highlight MiniStarterItemBullet guibg=NONE guifg=#7aa2f7
+           highlight MiniHipatterns_abb2bf_bg guibg=NONE guifg=#7e8294
+           highlight MiniHipatterns_61afef_bg guibg=NONE guifg=#7aa2f7
 
-          highlight CopilotChatHeader guifg=#7C3AED gui=bold
-          highlight CopilotChatSeparator guifg=#374151
+           highlight StatusLine guibg=NONE
+           highlight StatusLineNC guibg=NONE
+           highlight StatusLineTerm guibg=NONE
+           highlight StatusLineTermNC guibg=NONE
 
-          highlight MiniStarterItem guibg=NONE guifg=#d8dee9
-          highlight MiniStarterFooter guibg=NONE guifg=#81a1c1
-          highlight MiniStarterHeader guibg=NONE guifg=#88c0d0 gui=bold
-          highlight MiniStarterInactive guibg=NONE guifg=#4c566a
-          highlight MiniStarterItemBullet guibg=NONE guifg=#61afef
-          highlight MiniStarterItemPrefix guibg=NONE guifg=#a3be8c
-          highlight MiniStarterSection guibg=NONE guifg=#ebcb8b gui=bold
-          highlight MiniStarterQuery guibg=NONE guifg=#b48ead
+           highlight CopilotChatHeader guifg=#bb9af7 gui=bold
+           highlight CopilotChatSeparator guifg=#3b4261
 
-          highlight MiniStatuslineModeOther guifg=#b4befe guibg=NONE
-          highlight MiniStatuslineDevinfo guifg=#89b4fa guibg=NONE
-          highlight MiniStatuslineDiagnostics guifg=#f38ba8 guibg=NONE
-          highlight MiniStatuslineDiagnosticsError guifg=#f38ba8 guibg=NONE
-          highlight MiniStatuslineDiagnosticsWarn guifg=#f9e2af guibg=NONE
-          highlight MiniStatuslineDiagnosticsInfo guifg=#89b4fa guibg=NONE
-          highlight MiniStatuslineDiagnosticsHint guifg=#a6e3a1 guibg=NONE
-          highlight MiniStatuslineFilename guifg=#cdd6f4 guibg=NONE
-          highlight MiniStatuslineLocation guifg=#f38ba8 guibg=NONE
-          highlight MiniStatuslineRecording guifg=#f38ba8 guibg=NONE
-          highlight MiniStatuslineInactive guifg=#45475a guibg=NONE
-          highlight MiniStatuslineProgress guifg=#cba6f7 guibg=NONE
-          highlight MiniStatuslineHarpoon guifg=#fab387 guibg=NONE
-          highlight MiniStatuslineModified guifg=#e5c07b guibg=NONE
-        ]]
+           highlight MiniStarterItem guibg=NONE guifg=#c0caf5
+           highlight MiniStarterFooter guibg=NONE guifg=#7dcfff
+           highlight MiniStarterHeader guibg=NONE guifg=#7aa2f7 gui=bold
+           highlight MiniStarterInactive guibg=NONE guifg=#545c7e
+           highlight MiniStarterItemBullet guibg=NONE guifg=#7aa2f7
+           highlight MiniStarterItemPrefix guibg=NONE guifg=#9ece6a
+           highlight MiniStarterSection guibg=NONE guifg=#e0af68 gui=bold
+           highlight MiniStarterQuery guibg=NONE guifg=#bb9af7
+
+           highlight MiniStatuslineModeOther guifg=#bb9af7 guibg=NONE
+           highlight MiniStatuslineDevinfo guifg=#7dcfff guibg=NONE
+           highlight MiniStatuslineDiagnostics guifg=#f7768e guibg=NONE
+           highlight MiniStatuslineDiagnosticsError guifg=#f7768e guibg=NONE
+           highlight MiniStatuslineDiagnosticsWarn guifg=#e0af68 guibg=NONE
+           highlight MiniStatuslineDiagnosticsInfo guifg=#7dcfff guibg=NONE
+           highlight MiniStatuslineDiagnosticsHint guifg=#9ece6a guibg=NONE
+           highlight MiniStatuslineFilename guifg=#c0caf5 guibg=NONE
+           highlight MiniStatuslineLocation guifg=#f7768e guibg=NONE
+           highlight MiniStatuslineRecording guifg=#f7768e guibg=NONE
+           highlight MiniStatuslineInactive guifg=#545c7e guibg=NONE
+           highlight MiniStatuslineProgress guifg=#bb9af7 guibg=NONE
+           highlight MiniStatuslineHarpoon guifg=#e0af68 guibg=NONE
+           highlight MiniStatuslineModified guifg=#e0af68 guibg=NONE
+         ]]
     end,
   },
 }

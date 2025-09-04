ddb482d3b9d5cfce1461b1fe7623376636be0dca
Dwcks <corclan19@gmail.com>
2025-09-04 16:17:45 +0700

    added mappings for indent

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -140,6 +140,12 @@ return { -- Collection of various small independent plugins/modules
     require('mini.cursorword').setup()
     require('mini.statusline').setup()
     require('mini.indentscope').setup {
+      mappings = {
+        object_scope = 'ii', -- Select the inner scope of the current block
+        object_scope_with_border = 'ai', -- Select the entire scope including its borders
+        goto_top = '[i', -- Jump to the top border line of the current scope
+        goto_bottom = ']i', -- Jump to the bottom border line of the current scope
+      },
       symbol = '│',
       options = { try_as_border = true },
       draw = {

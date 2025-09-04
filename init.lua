6fe6247b3a02b56bc20a4ebd737b7501d800cbce
Dwcks <corclan19@gmail.com>
2025-09-04 12:30:18 +0700

    update text object keymaps and enable it in visual mode

--- init.lua
+++ init.lua
@@ -608,10 +608,27 @@ require('lazy').setup({
       textobjects = {
         move = {
           enable = true,
-          goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
-          goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
-          goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
-          goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+          set_jumps = true, -- Enable jumps for visual mode
+          goto_next_start = {
+            [']f'] = '@function.outer',
+            [']l'] = '@class.outer',
+            [']a'] = '@parameter.inner',
+          },
+          goto_next_end = {
+            [']F'] = '@function.outer',
+            [']L'] = '@class.outer',
+            [']A'] = '@parameter.inner',
+          },
+          goto_previous_start = {
+            ['[f'] = '@function.outer',
+            ['[c'] = '@class.outer',
+            ['[a'] = '@parameter.inner',
+          },
+          goto_previous_end = {
+            ['[F'] = '@function.outer',
+            ['[L'] = '@class.outer',
+            ['[A'] = '@parameter.inner',
+          },
         },
       },
     },

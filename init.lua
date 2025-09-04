9845e5eb876cbdeef18a0cf85bfae4fa33aa3c53
Dwcks <corclan19@gmail.com>
2025-09-04 14:38:32 +0700

    update neovide

--- init.lua
+++ init.lua
@@ -671,6 +671,7 @@ if vim.g.neovide then
   vim.g.neovide_padding_left = 0
   vim.g.neovide_padding_right = 0
   vim.g.neovide_padding_bottom = 0
-  vim.o.guifont = 'Google Sans Code:h14'
+  -- vim.o.guifont = 'Google Sans Code:h14'
+  vim.o.guifont = 'Monolisa Variable-Script:h14'
   vim.g.neovide_fullscreen = true
 end

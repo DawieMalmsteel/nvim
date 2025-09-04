672b805fa1aa383ff3d60d1fd427a580cd80670f
Dwcks <corclan19@gmail.com>
2025-09-04 14:13:25 +0700

    current

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
-  {
-    'polirritmico/monokai-nightasty.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      dark_style_background = 'transparent',
-      light_style_background = 'transparent',
-      hl_styles = {
-        floats = 'transparent',
-        sidebars = 'transparent',
-      },
-      markdown_header_marks = true,
-    },
-  },
+  -- {
+  --   'polirritmico/monokai-nightasty.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     dark_style_background = 'transparent',
+  --     light_style_background = 'transparent',
+  --     hl_styles = {
+  --       floats = 'transparent',
+  --       sidebars = 'transparent',
+  --     },
+  --     markdown_header_marks = true,
+  --   },
+  -- },
   {
     'tiagovla/tokyodark.nvim',
     lazy = false,
@@ -24,86 +24,69 @@ return {
       vim.cmd [[colorscheme tokyodark]]
     end,
   },
-  {
-    'zenbones-theme/zenbones.nvim',
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    -- dependencies = 'rktjmp/lush.nvim',
-    lazy = false,
-    priority = 1000,
-    -- you can set set configuration options here
-    config = function()
-      vim.g.zenbones_compat = 1
-      vim.g.zenbones_darken_comments = 45
-      -- vim.opt.background = 'light' -- hoặc 'dark'
-      vim.g.transparent_background = true
-      -- vim.cmd.colorscheme 'zenbones'
-    end,
-  },
-  {
-    'AlexvZyl/nordic.nvim',
-    config = function()
-      require('nordic').setup {
-        transparent = {
-          bg = true,
-          float = true,
-        },
-      }
-
-      -- vim.cmd.colorscheme 'nordic'
-      -- vim.cmd.colorscheme 'tokyodark'
-      -- vim.opt.background = 'light'
-      -- vim.cmd.colorscheme 'randomhue'
-
-      vim.cmd [[
-           highlight TabLine guibg=NONE guifg=#7e8294
-           highlight TabLineSel guibg=#7aa2f7 guifg=#1a1b26
-           highlight TabLineFill guibg=NONE guifg=#3b4261
-
-           highlight MiniTablineCurrent guibg=NONE guifg=#7aa2f7
-           highlight MiniTablineVisible guibg=NONE guifg=#7e8294
-           highlight MiniTablineHidden guibg=NONE guifg=#3b4261
-
-           highlight MiniTablineModifiedCurrent guibg=NONE guifg=#e0af68
-           highlight MiniTablineModifiedVisible guibg=NONE guifg=#f7768e
-           highlight MiniTablineModifiedHidden guibg=NONE guifg=#e0af68
-           highlight MiniTablineTabpagesection guibg=NONE guifg=#9ece6a
-           highlight MiniTablineFill guibg=NONE guifg=#3b4261
-           highlight MiniTablineTrunc guibg=NONE guifg=#3b4261
-           highlight MiniStarterItemBullet guibg=NONE guifg=#7aa2f7
-           highlight MiniHipatterns_abb2bf_bg guibg=NONE guifg=#7e8294
-           highlight MiniHipatterns_61afef_bg guibg=NONE guifg=#7aa2f7
-
-           highlight StatusLine guibg=NONE
-           highlight StatusLineNC guibg=NONE
-           highlight StatusLineTerm guibg=NONE
-           highlight StatusLineTermNC guibg=NONE
-
-           highlight MiniStarterItem guibg=NONE guifg=#c0caf5
-           highlight MiniStarterFooter guibg=NONE guifg=#7dcfff
-           highlight MiniStarterHeader guibg=NONE guifg=#7aa2f7 gui=bold
-           highlight MiniStarterInactive guibg=NONE guifg=#545c7e
-           highlight MiniStarterItemBullet guibg=NONE guifg=#7aa2f7
-           highlight MiniStarterItemPrefix guibg=NONE guifg=#9ece6a
-           highlight MiniStarterSection guibg=NONE guifg=#e0af68 gui=bold
-           highlight MiniStarterQuery guibg=NONE guifg=#bb9af7
-
-           highlight MiniStatuslineModeOther guifg=#bb9af7 guibg=NONE
-           highlight MiniStatuslineDevinfo guifg=#7dcfff guibg=NONE
-           highlight MiniStatuslineDiagnostics guifg=#f7768e guibg=NONE
-           highlight MiniStatuslineDiagnosticsError guifg=#f7768e guibg=NONE
-           highlight MiniStatuslineDiagnosticsWarn guifg=#e0af68 guibg=NONE
-           highlight MiniStatuslineDiagnosticsInfo guifg=#7dcfff guibg=NONE
-           highlight MiniStatuslineDiagnosticsHint guifg=#9ece6a guibg=NONE
-           highlight MiniStatuslineFilename guifg=#c0caf5 guibg=NONE
-           highlight MiniStatuslineLocation guifg=#f7768e guibg=NONE
-           highlight MiniStatuslineRecording guifg=#f7768e guibg=NONE
-           highlight MiniStatuslineInactive guifg=#545c7e guibg=NONE
-           highlight MiniStatuslineProgress guifg=#bb9af7 guibg=NONE
-           highlight MiniStatuslineHarpoon guifg=#e0af68 guibg=NONE
-           highlight MiniStatuslineModified guifg=#e0af68 guibg=NONE
-         ]]
-    end,
-  },
+  -- {
+  --   'AlexvZyl/nordic.nvim',
+  --   config = function()
+  --     require('nordic').setup {
+  --       transparent = {
+  --         bg = true,
+  --         float = true,
+  --       },
+  --     }
+  --
+  --     -- vim.cmd.colorscheme 'nordic'
+  --     -- vim.cmd.colorscheme 'tokyodark'
+  --     -- vim.opt.background = 'light'
+  --     -- vim.cmd.colorscheme 'randomhue'
+  --
+  --     vim.cmd [[
+  --          highlight TabLine guibg=NONE guifg=#7e8294
+  --          highlight TabLineSel guibg=#7aa2f7 guifg=#1a1b26
+  --          highlight TabLineFill guibg=NONE guifg=#3b4261
+  --
+  --          highlight MiniTablineCurrent guibg=NONE guifg=#7aa2f7
+  --          highlight MiniTablineVisible guibg=NONE guifg=#7e8294
+  --          highlight MiniTablineHidden guibg=NONE guifg=#3b4261
+  --
+  --          highlight MiniTablineModifiedCurrent guibg=NONE guifg=#e0af68
+  --          highlight MiniTablineModifiedVisible guibg=NONE guifg=#f7768e
+  --          highlight MiniTablineModifiedHidden guibg=NONE guifg=#e0af68
+  --          highlight MiniTablineTabpagesection guibg=NONE guifg=#9ece6a
+  --          highlight MiniTablineFill guibg=NONE guifg=#3b4261
+  --          highlight MiniTablineTrunc guibg=NONE guifg=#3b4261
+  --          highlight MiniStarterItemBullet guibg=NONE guifg=#7aa2f7
+  --          highlight MiniHipatterns_abb2bf_bg guibg=NONE guifg=#7e8294
+  --          highlight MiniHipatterns_61afef_bg guibg=NONE guifg=#7aa2f7
+  --
+  --          highlight StatusLine guibg=NONE
+  --          highlight StatusLineNC guibg=NONE
+  --          highlight StatusLineTerm guibg=NONE
+  --          highlight StatusLineTermNC guibg=NONE
+  --
+  --          highlight MiniStarterItem guibg=NONE guifg=#c0caf5
+  --          highlight MiniStarterFooter guibg=NONE guifg=#7dcfff
+  --          highlight MiniStarterHeader guibg=NONE guifg=#7aa2f7 gui=bold
+  --          highlight MiniStarterInactive guibg=NONE guifg=#545c7e
+  --          highlight MiniStarterItemBullet guibg=NONE guifg=#7aa2f7
+  --          highlight MiniStarterItemPrefix guibg=NONE guifg=#9ece6a
+  --          highlight MiniStarterSection guibg=NONE guifg=#e0af68 gui=bold
+  --          highlight MiniStarterQuery guibg=NONE guifg=#bb9af7
+  --
+  --          highlight MiniStatuslineModeOther guifg=#bb9af7 guibg=NONE
+  --          highlight MiniStatuslineDevinfo guifg=#7dcfff guibg=NONE
+  --          highlight MiniStatuslineDiagnostics guifg=#f7768e guibg=NONE
+  --          highlight MiniStatuslineDiagnosticsError guifg=#f7768e guibg=NONE
+  --          highlight MiniStatuslineDiagnosticsWarn guifg=#e0af68 guibg=NONE
+  --          highlight MiniStatuslineDiagnosticsInfo guifg=#7dcfff guibg=NONE
+  --          highlight MiniStatuslineDiagnosticsHint guifg=#9ece6a guibg=NONE
+  --          highlight MiniStatuslineFilename guifg=#c0caf5 guibg=NONE
+  --          highlight MiniStatuslineLocation guifg=#f7768e guibg=NONE
+  --          highlight MiniStatuslineRecording guifg=#f7768e guibg=NONE
+  --          highlight MiniStatuslineInactive guifg=#545c7e guibg=NONE
+  --          highlight MiniStatuslineProgress guifg=#bb9af7 guibg=NONE
+  --          highlight MiniStatuslineHarpoon guifg=#e0af68 guibg=NONE
+  --          highlight MiniStatuslineModified guifg=#e0af68 guibg=NONE
+  --        ]]
+  --   end,
+  -- },
 }

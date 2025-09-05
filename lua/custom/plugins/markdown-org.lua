0df0984e1805974bbdadc5679a14bb0e28d61e55
Dwcks <corclan19@gmail.com>
2025-09-05 12:38:55 +0700

    added markdown-org like orgmode in emacs

---
+++ lua/custom/plugins/markdown-org.lua
@@ -0,0 +1,27 @@
+return {
+  'Kurama622/markdown-org',
+  ft = 'markdown',
+  lazy = false,
+  config = function()
+    vim.g.language_path = {
+      python = 'python',
+      python3 = 'python3',
+      javascript = 'node',
+      go = 'go',
+      c = 'gcc -Wall',
+      cpp = 'g++ -std=c++11 -Wall',
+      bash = 'bash',
+      ['c++'] = 'g++ -std=c++11 -Wall',
+    }
+    return {
+      default_quick_keys = 0,
+      vim.api.nvim_set_var('org#style#border', 2),
+      vim.api.nvim_set_var('org#style#bordercolor', 'FloatBorder'),
+      vim.api.nvim_set_var('org#style#color', 'String'),
+    }
+  end,
+  keys = {
+    { '<leader>Mr', '<cmd>call org#main#runCodeBlock()<cr>' },
+    { '<leader>Ml', '<cmd>call org#main#runLanguage()<cr>' },
+  },
+}

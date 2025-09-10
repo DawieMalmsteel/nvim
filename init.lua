af45f1ef50a38e469fc2fa94de053be2217421f0
dwcks <corclan19@gmail.com>
2025-09-10 23:41:25 +0700

    refactor: extract highlight cmds to core module
    Move custom highlight definitions from custom/plugins/colorscheme.lua
    into lua/core/colors_cmd.lua and require it from init.lua.
    
    Decouples highlights from the colorscheme plugin, applies them on
    startup, and centralizes theme tweaks in one place. No functional
    changes intended.

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ require 'core.autocmds'
 require 'core.lazy'
 require 'core.misc'
 require 'core.neovide'
+require 'core.colors_cmd'
 -- User keymaps (kept separate as before)
 require 'custom.keymaps'
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -22,36 +22,6 @@ return {
     config = function(_, opts)
       require('tokyodark').setup(opts) -- calling setup is optional
       vim.cmd [[colorscheme tokyodark]]
-
-      vim.cmd [[
-           highlight MiniTablineModifiedCurrent guifg=#e0af68
-           highlight MiniTablineModifiedVisible guifg=#f7768e
-           highlight MiniTablineModifiedHidden guifg=#e0af68
-           highlight MiniTablineTabpagesection guifg=#9ece6a
-           highlight MiniTablineFill guifg=#3b4261
-           highlight MiniTablineTrunc guifg=#3b4261
-           highlight MiniStarterItemBullet guifg=#7aa2f7
-           highlight MiniHipatterns_abb2bf_bg guifg=#7e8294
-           highlight MiniHipatterns_61afef_bg guifg=#7aa2f7
-
-           highlight StatusLine guibg=NONE
-           highlight StatusLineNC guibg=NONE
-
-           highlight MiniStatuslineModeOther guifg=#bb9af7 guibg=NONE
-           highlight MiniStatuslineDevinfo guifg=#7dcfff guibg=NONE
-           highlight MiniStatuslineDiagnostics guifg=#f7768e guibg=NONE
-           highlight MiniStatuslineDiagnosticsError guifg=#f7768e guibg=NONE
-           highlight MiniStatuslineDiagnosticsWarn guifg=#e0af68 guibg=NONE
-           highlight MiniStatuslineDiagnosticsInfo guifg=#7dcfff guibg=NONE
-           highlight MiniStatuslineDiagnosticsHint guifg=#9ece6a guibg=NONE
-           highlight MiniStatuslineFilename guifg=#c0caf5 guibg=NONE
-           highlight MiniStatuslineLocation guifg=#f7768e guibg=NONE
-           highlight MiniStatuslineRecording guifg=#f7768e guibg=NONE
-           highlight MiniStatuslineInactive guifg=#545c7e guibg=NONE
-           highlight MiniStatuslineProgress guifg=#bb9af7 guibg=NONE
-           highlight MiniStatuslineHarpoon guifg=#9ece6a guibg=NONE
-           highlight MiniStatuslineModified guifg=#e0af68 guibg=NONE
-         ]]
     end,
   },
   -- {

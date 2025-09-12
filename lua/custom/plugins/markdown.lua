19285290df86e2a2453557aa6f6f0d3b926581b0
dwcks <corclan19@gmail.com>
2025-09-12 19:11:33 +0700

    feat: add CodeCompanion plugin and update markdown
    Add codecompanion.nvim with plenary dependency. Extend render-markdown
    plugin filetypes to include 'codecompanion' and 'llm' so AI interaction
    buffers get proper markdown rendering.

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -5,4 +5,5 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   opts = {},
+  ft = { 'markdown', 'codecompanion', 'llm' },
 }

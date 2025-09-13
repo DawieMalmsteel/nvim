f557a05fc92494f815253504f8f42b6f91bde9cc
dwcks <corclan19@gmail.com>
2025-09-13 20:57:46 +0700

    feat(keymaps): toggle mini statusline/tabline
    Add <leader>ts to toggle mini.statusline via g:ministatusline_disable and
    force status redraw. Add <leader>tt to toggle mini.tabline plus native
    'showtabline', preserving previous value, with user notifications. No
    breaking changes.

--- lua/custom/keymaps/toggle.lua
+++ lua/custom/keymaps/toggle.lua
@@ -20,3 +20,30 @@ map('n', '<leader>tc', '<CMD>TSContext toggle<CR>', { desc = 'Toggle Treesitter 
 map('n', '<leader>tD', function()
   Snacks.dashboard()
 end, { desc = 'Toggle Dashboard' })
+
+map('n', '<leader>ts', function()
+  if vim.g.ministatusline_disable then
+    vim.g.ministatusline_disable = nil
+    vim.notify('Mini statusline: ON', vim.log.levels.INFO)
+  else
+    vim.g.ministatusline_disable = true
+    vim.notify('Mini statusline: OFF', vim.log.levels.INFO)
+  end
+  vim.cmd.redrawstatus()
+end, { desc = 'Toggle Mini Statusline' })
+
+-- Toggle mini.tabline (also hide actual tabline UI)
+map('n', '<leader>tt', function()
+  if vim.g.minitabline_disable then
+    vim.g.minitabline_disable = nil
+    local prev = vim.g.__showtabline_before_mini_toggle
+    vim.o.showtabline = (prev and prev >= 0) and prev or 1
+    vim.notify('Mini tabline: ON', vim.log.levels.INFO)
+  else
+    vim.g.minitabline_disable = true
+    vim.g.__showtabline_before_mini_toggle = vim.o.showtabline
+    vim.o.showtabline = 0
+    vim.notify('Mini tabline: OFF', vim.log.levels.INFO)
+  end
+  vim.cmd 'redraw!'
+end, { desc = 'Toggle Mini Tabline' })

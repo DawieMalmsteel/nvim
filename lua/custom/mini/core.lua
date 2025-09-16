f1d122729aede05ecd8dbc432c64efb405db9095
dwcks <corclan19@gmail.com>
2025-09-16 18:42:06 +0700

    feat(ui): bật nền trong suốt; tắt indentscope
    Bật nền trong suốt cho giao diện tokyodark (transparent_background = true)
    để đồng bộ tốt hơn với terminal/compositor. Vô hiệu hóa module Mini
    Indentscope bằng cách comment để bỏ hiển thị đường dẫn thụt dòng, giúp
    giao diện gọn hơn và giảm nhiễu thị giác.

--- lua/custom/mini/core.lua
+++ lua/custom/mini/core.lua
@@ -2,7 +2,7 @@ local modules = {
   'custom.mini.editing.ai',
   'custom.mini.editing.surround',
   'custom.mini.editing.pairs',
-  'custom.mini.editing.indentscope',
+  -- 'custom.mini.editing.indentscope',
   'custom.mini.editing.bracketed',
   'custom.mini.editing.bufremove',
   'custom.mini.ui.icons',

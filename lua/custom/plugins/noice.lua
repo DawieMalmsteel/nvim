c12b616c54510e68a3bfbb28c954d0bc64a0ce37
dwcks <corclan19@gmail.com>
2025-09-24 12:36:00 +0700

    feat(noice): enhance UI with custom views and cmdline formats
    Added custom configurations for `cmdline_popup` and `popupmenu` views
    to improve the user interface. Configurations include border styles,
    padding, position, size, and window options.
    
    Introduced custom `cmdline` formats for `replace` and `replace_all`
    commands with icons and regex support. Commented out the `routes`
    section for hiding search count messages.

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,7 +1,72 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+    views = {
+      cmdline_popup = {
+        border = {
+          style = 'none',
+          padding = { 1, 2 },
+        },
+        position = {
+          row = 15,
+          col = '50%',
+        },
+        size = {
+          width = 60,
+          height = 'auto',
+        },
+        win_options = {
+          winhighlight = { NormalFloat = 'NormalFloat' },
+        },
+      },
+      popupmenu = {
+        relative = 'editor',
+        position = {
+          row = 3,
+          col = '50%',
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = 'none',
+          padding = { 1, 2 },
+        },
+      },
+    },
+
+    -- hide the search count message
+    -- routes = {
+    --   {
+    --     filter = {
+    --       event = 'msg_show',
+    --       kind = 'search_count',
+    --     },
+    --     opts = { skip = true },
+    --   },
+    -- },
+
+    cmdline = {
+      format = {
+        replace = {
+          conceal = false,
+          pattern = '^:s/',
+          icon = ' ',
+          lang = 'regex',
+          view = 'cmdline',
+        },
+        replace_all = {
+          conceal = false,
+          pattern = '^:%%s/',
+          icon = ' 󰬳',
+          lang = 'regex',
+          view = 'cmdline',
+        },
+      },
+    },
+  },
   dependencies = {
     'MunifTanjim/nui.nvim',
   },

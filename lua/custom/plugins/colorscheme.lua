197f895c91852c7f716c1650569505d4f1be2a1e
dwcks <corclan19@gmail.com>
2025-09-25 01:29:04 +0700

    deleted unneed for hackathon

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,27 +1,13 @@
 return {
   {
-    'tiagovla/tokyodark.nvim',
-    lazy = false,
-    opts = {
-      transparent_background = true,
-    },
-    config = function(_, opts)
-      require('tokyodark').setup(opts) -- calling setup is optional
-      -- vim.cmd [[colorscheme tokyodark]]
-    end,
-  },
-  {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     opts = {
       transparent = true,
     },
-  },
-  {
-    'AlexvZyl/nordic.nvim',
     config = function()
-      require('nordic').setup {
+      require('tokyonight').setup {
         transparent = {
           bg = true,
           float = true,

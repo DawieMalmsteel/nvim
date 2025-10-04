794bb7d65fb2239b4c82dd8ec2d958282a80893f
dwcks <corclan19@gmail.com>
2025-10-04 16:00:54 +0700

    feat(plugins): comment out blink-cmp-copilot integration
    The `giuxtaposition/blink-cmp-copilot` plugin and its related
    configuration have been commented out. This change temporarily
    disables the Copilot integration in the Blink plugin.
    
    - Commented out the plugin dependency in `dependencies`.
    - Commented out the `copilot` provider configuration.
    
    This adjustment may be revisited in the future if the integration
    is needed again or requires updates.

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
   event = 'VimEnter',
   version = '1.*',
   dependencies = {
-    'giuxtaposition/blink-cmp-copilot',
+    -- 'giuxtaposition/blink-cmp-copilot',
     'rafamadriz/friendly-snippets',
     'folke/lazydev.nvim',
     {
@@ -45,21 +45,21 @@ return {
       default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'copilot' },
       providers = {
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-        copilot = {
-          name = 'copilot',
-          module = 'blink-cmp-copilot',
-          score_offset = 100,
-          async = true,
-          transform_items = function(_, items)
-            local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
-            local kind_idx = #CompletionItemKind + 1
-            CompletionItemKind[kind_idx] = 'Copilot'
-            for _, item in ipairs(items) do
-              item.kind = kind_idx
-            end
-            return items
-          end,
-        },
+        -- copilot = {
+        --   name = 'copilot',
+        --   module = 'blink-cmp-copilot',
+        --   score_offset = 100,
+        --   async = true,
+        --   transform_items = function(_, items)
+        --     local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+        --     local kind_idx = #CompletionItemKind + 1
+        --     CompletionItemKind[kind_idx] = 'Copilot'
+        --     for _, item in ipairs(items) do
+        --       item.kind = kind_idx
+        --     end
+        --     return items
+        --   end,
+        -- },
       },
       appearance = {
         kind_icons = {

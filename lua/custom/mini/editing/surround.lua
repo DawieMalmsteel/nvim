12818298223d0cdeb2453b08c52f6a46de1f9cd1
dwcks <corclan19@gmail.com>
2025-10-08 23:33:22 +0700

    feat(editing): enhance surround keymaps and setup
    Refactor `mini.surround` configuration to include detailed keymap
    descriptions and updated mappings. Added support for highlighting
    surroundings and updating `n_lines` dynamically. Improved usability
    with descriptive keymap labels for better discoverability.

--- lua/custom/mini/editing/surround.lua
+++ lua/custom/mini/editing/surround.lua
@@ -1,9 +1,28 @@
 local M = function()
   -- Add/delete/replace surroundings (brackets, quotes, etc.)
-  --
-  -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-  -- - sd'   - [S]urround [D]elete [']quotes
-  -- - sr)'  - [S]urround [R]eplace [)] [']
-  require('mini.surround').setup()
+
+  -- Keymap descriptions
+  local map = vim.keymap.set
+  map({ 'n', 'v' }, 'gsa', '', { desc = 'Add Surrounding' })
+  map('n', 'gsd', '', { desc = 'Delete Surrounding' })
+  map('n', 'gsf', '', { desc = 'Find Right Surrounding' })
+  map('n', 'gsF', '', { desc = 'Find Left Surrounding' })
+  map('n', 'gsh', '', { desc = 'Highlight Surrounding' })
+  map('n', 'gsr', '', { desc = 'Replace Surrounding' })
+  map('n', 'gsn', '', { desc = 'Update `MiniSurround.config.n_lines`' })
+
+  require('mini.surround').setup {
+    highlight_duration = 100,
+    mappings = {
+      add = 'gsa', -- Add surrounding in Normal and Visual modes
+      delete = 'gsd', -- Delete surrounding
+      find = 'gsf', -- Find surrounding (to the right)
+      find_left = 'gsF', -- Find surrounding (to the left)
+      highlight = 'gsh', -- Highlight surrounding
+      replace = 'gsr', -- Replace surrounding
+      update_n_lines = 'gsn', -- Update `n_lines`
+    },
+  }
 end
+
 return M

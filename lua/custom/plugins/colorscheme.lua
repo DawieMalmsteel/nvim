e481992134b8e8257274bee358542ff1f801ab8a
dwcks <corclan19@gmail.com>
2025-10-15 00:24:26 +0700

    current

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -30,7 +30,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('github-theme').setup {
-        options = { transparent = true },
+        options = { transparent = false },
       }
 
       vim.cmd 'colorscheme github_dark_tritanopia'

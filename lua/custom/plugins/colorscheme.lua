c9060ab310ce5fc733b69bc699c94b63fa07c140
dwcks <corclan19@gmail.com>
2025-11-06 22:39:12 +0700

    update color

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -7,9 +7,11 @@ return {
     },
     config = function(_, opts)
       require('tokyodark').setup(opts) -- calling setup is optional
-      vim.cmd [[colorscheme tokyodark]]
+      -- vim.cmd [[colorscheme tokyodark]]
+      vim.cmd [[colorscheme nightfly]]
     end,
   },
+  { 'bluz71/vim-nightfly-colors', name = 'nightfly', lazy = false, priority = 1000 },
   {
     -- 'sainnhe/sonokai',
     -- lazy = false,

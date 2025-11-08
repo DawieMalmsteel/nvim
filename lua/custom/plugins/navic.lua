0369b3e4ec764e9eff10cf9ee6dc9db648976c2c
dwcks <corclan19@gmail.com>
2025-11-09 01:46:21 +0700

    added navic and navic buddy

---
+++ lua/custom/plugins/navic.lua
@@ -0,0 +1,10 @@
+return {
+  'SmiteshP/nvim-navbuddy',
+  dependencies = {
+    'SmiteshP/nvim-navic',
+  },
+  opts = {
+    window = { size = '40%', position = { row = '100%', col = '100%' } },
+    lsp = { auto_attach = true },
+  },
+}

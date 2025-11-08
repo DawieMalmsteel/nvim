02855a1cc3c22108c5105848f9e0fc7cfbae5a8c
dwcks <corclan19@gmail.com>
2025-11-09 02:11:45 +0700

    changed to dropbar

---
+++ lua/custom/plugins/dropbar.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      local dropbar_api = require 'dropbar.api'
+      vim.keymap.set('n', '<leader>,', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '<leader>cn', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[,', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '],', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
+  },
+}
+
+-- return {
+--   'SmiteshP/nvim-navbuddy',
+--   dependencies = {
+--     'SmiteshP/nvim-navic',
+--   },
+--   opts = {
+--     window = { size = '40%', position = { row = '100%', col = '100%' } },
+--     lsp = { auto_attach = true },
+--   },
+-- }

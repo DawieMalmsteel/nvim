15c617adb5d2153cf2e8ca3a1bebc344f91db8b9
dwcks <corclan19@gmail.com>
2025-11-10 16:58:02 +0700

    changed leader , to leader tab btw

--- lua/custom/plugins/dropbar.lua
+++ lua/custom/plugins/dropbar.lua
@@ -7,7 +7,7 @@ return {
     },
     config = function()
       local dropbar_api = require 'dropbar.api'
-      vim.keymap.set('n', '<leader>,', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '<leader><tab>', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
       vim.keymap.set('n', '[<tab>', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
       vim.keymap.set('n', '<leader>cN', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
       vim.keymap.set('n', ']<tab>', dropbar_api.select_next_context, { desc = 'Select next context' })

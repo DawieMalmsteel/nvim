31bc36c6ea9e318105b3fcb1d277024eef7ec32a
dwcks <corclan19@gmail.com>
2025-11-14 02:46:21 +0700

    current

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -8,6 +8,7 @@ return {
     config = function(_, opts)
       require('tokyodark').setup(opts) -- calling setup is optional
       -- vim.cmd [[colorscheme tokyodark]]
+      vim.g.nord_disable_background = true
       vim.cmd [[colorscheme nord]]
       -- vim.cmd [[colorscheme nightfly]]
     end,

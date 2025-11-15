95918dd507822b4820ccbc01cf20a9e1b6434098
dwcks <corclan19@gmail.com>
2025-11-15 21:04:59 +0700

    use snacks at default picker

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -3,7 +3,7 @@ return {
     'folke/snacks.nvim',
     opts = {
       picker = {
-        ui_select = false,
+        ui_select = true,
         previewers = {
           git = {
             native = true,

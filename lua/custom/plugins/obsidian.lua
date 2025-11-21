cf6a758336772f3dfe10c1ce8d3273475424113d
dwcks <corclan19@gmail.com>
2025-11-22 00:06:59 +0700

    added obsidian

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,28 @@
+return {
+  'obsidian-nvim/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  event = {
+    'BufReadPre',
+  },
+  opts = {
+    legacy_commands = false,
+    workspaces = {
+      {
+        name = 'Main vault',
+        path = '~/Documents/Obsidian Vault/',
+      },
+      {
+        name = 'Random thoughts',
+        path = '~/Documents/Random in my mind/',
+      },
+      {
+        name = 'Notes',
+        path = '~/Documents/Notes/',
+      },
+    },
+
+    -- see below for full list of options 👇
+  },
+}

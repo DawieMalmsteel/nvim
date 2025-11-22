2c27d8ba0cd4ff1565e26ca34171e4bc0c11210d
dwcks <corclan19@gmail.com>
2025-11-23 00:53:06 +0700

    added hex editor to nvim

---
+++ lua/custom/plugins/hex.lua
@@ -0,0 +1,9 @@
+return {
+  'RaafatTurki/hex.nvim',
+  config = function()
+    -- NOTE: This plugin requires vim install first or xxd-standalone
+    require('hex').setup {
+      -- Configuration options go here
+    }
+  end,
+}

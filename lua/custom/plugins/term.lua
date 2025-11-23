f5271abc346ee4710cb6bb219d683cb72b3afca8
dwcks <corclan19@gmail.com>
2025-11-23 14:53:43 +0700

    refactor

---
+++ lua/custom/plugins/term.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'nvzone/floaterm',
+    dependencies = 'nvzone/volt',
+    opts = {
+      terminals = {
+        { name = 'Terminal', cmd = 'fish' },
+      },
+    },
+    cmd = 'FloatermToggle',
+  },
+}

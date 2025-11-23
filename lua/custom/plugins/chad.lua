e4776362642c7b61c408f269ee5c4775d2d5ae71
dwcks <corclan19@gmail.com>
2025-11-23 14:51:35 +0700

    refactor code

--- lua/custom/plugins/chad.lua
+++ lua/custom/plugins/chad.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    'nvzone/showkeys',
-    cmd = 'ShowkeysToggle',
-    opts = {
-      show_count = true,
-
-      -- bottom-left, bottom-right, bottom-center, top-left, top-right, top-center
-      -- position = 'bottom-right',
-    },
-  },
-  {
-    'nvzone/typr',
-    dependencies = 'nvzone/volt',
-    opts = {},
-    cmd = { 'Typr', 'TyprStats' },
-  },
+  -- {
+  --   'nvzone/showkeys',
+  --   cmd = 'ShowkeysToggle',
+  --   opts = {
+  --     show_count = true,
+  --
+  --     -- bottom-left, bottom-right, bottom-center, top-left, top-right, top-center
+  --     -- position = 'bottom-right',
+  --   },
+  -- },
+  -- {
+  --   'nvzone/typr',
+  --   dependencies = 'nvzone/volt',
+  --   opts = {},
+  --   cmd = { 'Typr', 'TyprStats' },
+  -- },
   {
     'nvzone/floaterm',
     dependencies = 'nvzone/volt',

41f0f26cfed0832142a54aa1c3c631e73806fe4b
dwcks <corclan19@gmail.com>
2025-11-27 14:40:04 +0700

    changed leader o to leader O

--- lua/custom/keymaps/explorer/snacks.lua
+++ lua/custom/keymaps/explorer/snacks.lua
@@ -5,4 +5,4 @@ map('n', '<leader>E', function()
   local Snacks = require 'snacks'
   Snacks.explorer()
 end, { desc = 'Explorer Snacks (cwd)' })
-map('n', '<leader>o', '<CMD>Pick oldfiles<CR>', { desc = 'Open oldfiles' })
+map('n', '<leader>O', '<CMD>Pick oldfiles<CR>', { desc = 'Open oldfiles' })

9cd0bb981a8b8feaa4b4ed4660cfd199c4923a1b
dwcks <corclan19@gmail.com>
2025-12-02 00:13:34 +0700

    disable background

--- lua/custom/plugins/term.lua
+++ lua/custom/plugins/term.lua
@@ -1,5 +1,14 @@
 return {
   {
+    'nvzone/showkeys',
+    cmd = 'ShowkeysToggle',
+    opts = {
+      show_count = false,
+      -- bottom-left, bottom-right, bottom-center, top-left, top-right, top-center
+      position = 'bottom-right',
+    },
+  },
+  {
     'nvzone/floaterm',
     dependencies = 'nvzone/volt',
     opts = {

ec7e2610306401c386f0ecfe695cd431c1873516
dwcks <corclan19@gmail.com>
2025-12-03 17:52:47 +0700

    added tsgo removee vtsls

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -178,6 +178,8 @@ return {
         },
       },
 
+      tsgo = {},
+
       roslyn = {
         settings = {
           ['csharp|inlay_hints'] = {
@@ -250,41 +252,41 @@ return {
       -- jdtls = {},
       solidity_ls = {},
       -- harper_ls = {},
-      vtsls = {
-        filetypes = {
-          'javascript',
-          'javascriptreact',
-          'javascript.jsx',
-          'typescript',
-          'typescriptreact',
-          'typescript.tsx',
-        },
-        settings = {
-          complete_function_calls = true,
-          vtsls = {
-            enableMoveToFileCodeAction = true,
-            autoUseWorkspaceTsdk = true,
-            experimental = {
-              maxInlayHintLength = 30,
-              completion = {
-                enableServerSideFuzzyMatch = true,
-              },
-            },
-          },
-          typescript = {
-            updateImportsOnFileMove = { enabled = 'always' },
-            suggest = { completeFunctionCalls = true },
-            inlayHints = {
-              enumMemberValues = { enabled = true },
-              functionLikeReturnTypes = { enabled = true },
-              parameterNames = { enabled = 'literals' },
-              parameterTypes = { enabled = true },
-              propertyDeclarationTypes = { enabled = true },
-              variableTypes = { enabled = true },
-            },
-          },
-        },
-      },
+      -- vtsls = {
+      --   filetypes = {
+      --     'javascript',
+      --     'javascriptreact',
+      --     'javascript.jsx',
+      --     'typescript',
+      --     'typescriptreact',
+      --     'typescript.tsx',
+      --   },
+      --   settings = {
+      --     complete_function_calls = true,
+      --     vtsls = {
+      --       enableMoveToFileCodeAction = true,
+      --       autoUseWorkspaceTsdk = true,
+      --       experimental = {
+      --         maxInlayHintLength = 30,
+      --         completion = {
+      --           enableServerSideFuzzyMatch = true,
+      --         },
+      --       },
+      --     },
+      --     typescript = {
+      --       updateImportsOnFileMove = { enabled = 'always' },
+      --       suggest = { completeFunctionCalls = true },
+      --       inlayHints = {
+      --         enumMemberValues = { enabled = true },
+      --         functionLikeReturnTypes = { enabled = true },
+      --         parameterNames = { enabled = 'literals' },
+      --         parameterTypes = { enabled = true },
+      --         propertyDeclarationTypes = { enabled = true },
+      --         variableTypes = { enabled = true },
+      --       },
+      --     },
+      --   },
+      -- },
       tailwindcss = {
         root_dir = function(...)
           return require('lspconfig.util').root_pattern '.git'(...)

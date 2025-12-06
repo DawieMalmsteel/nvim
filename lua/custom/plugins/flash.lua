3ef5d8cabe5f8b0d944d2d5b54575cb29bac2b4b
dwcks <corclan19@gmail.com>
2025-12-06 10:48:18 +0700

    current

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -2,11 +2,23 @@ return {
   'folke/flash.nvim',
   event = 'VeryLazy',
   vscode = true,
-  ---@type Flash.Config
   opts = {},
-  -- stylua: ignore
   keys = {
-        { "S", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "R", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    {
+      'S',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash',
+    },
+    {
+      'R',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
   },
 }

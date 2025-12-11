a7b09d5d2f83ad9def5cd3abe3a490e4a402527e
dwcks <corclan19@gmail.com>
2025-12-11 17:56:40 +0700

    current

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
       'mason-org/mason.nvim',
       opts = {
         ui = {
-          border = 'rounded',
+          border = 'single',
           icons = {
             package_installed = '󰄳 ',
             package_pending = '󰑓 ',

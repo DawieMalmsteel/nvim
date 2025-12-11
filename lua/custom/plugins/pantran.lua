571c2b3968b8d7ff49335903031a6f721c37538f
dwcks <corclan19@gmail.com>
2025-12-11 18:44:54 +0700

    added google translate to nvim

---
+++ lua/custom/plugins/pantran.lua
@@ -0,0 +1,12 @@
+return {
+  'potamides/pantran.nvim',
+  opts = {
+    default_engine = 'google',
+    engines = {
+      google = {
+        default_source = 'auto',
+        default_target = 'vi',
+      },
+    },
+  },
+}

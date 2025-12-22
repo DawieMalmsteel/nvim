a12adf5c1c7697281c9b13a59934054a04ab2865
dwcks <corclan19@gmail.com>
2025-12-22 22:50:44 +0700

    disable ui select

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -3,7 +3,7 @@ return {
     'folke/snacks.nvim',
     opts = {
       picker = {
-        ui_select = true,
+        ui_select = false,
         previewers = {
           git = {
             native = true,

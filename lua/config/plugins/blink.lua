358d4e3c5cb68a9abc2f8854b4ba7accc972f88c
dwcks <corclan19@gmail.com>
2025-12-22 22:49:38 +0700

    fix config error

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -43,7 +43,7 @@ return {
       keymap = { preset = 'default' },
       completion = {
         menu = { auto_show = true },
-        completion = { ghost_text = { enabled = false } },
+        ghost_text = { enabled = true },
       },
     },
     sources = {

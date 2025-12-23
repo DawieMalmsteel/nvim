435d5e6c2529b7d5fa4704e1742d7c294bd150f2
dwcks <corclan19@gmail.com>
2025-12-23 17:26:30 +0700

    bring auto show docs back

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -35,7 +35,7 @@ return {
     },
     appearance = { nerd_font_variant = 'mono' },
     completion = {
-      documentation = { auto_show = false, auto_show_delay_ms = 0 },
+      documentation = { auto_show = true, auto_show_delay_ms = 0 },
       ghost_text = { enabled = vim.g.ai_cmp },
     },
     snippets = { preset = 'mini_snippets' },

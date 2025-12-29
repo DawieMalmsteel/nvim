01b698dce36b563e2ce82ebaa63f815ef2cadd6c
dwcks <corclan19@gmail.com>
2025-12-29 13:15:40 +0700

    update keymaps

--- lua/custom/plugins/gtranslate.lua
+++ lua/custom/plugins/gtranslate.lua
@@ -1,10 +1,15 @@
 return {
-  dir = '/home/dwcks/Projects/gtranslate',
+  dir = '~/Projects/gtranslate',
   -- this is for local plugin development
   name = 'gtranslate',
   config = function()
     require('gtranslate').setup {
       target_lang = 'vi',
     }
+
+    -- Dịch nhanh bằng Google
+    vim.keymap.set('v', '<leader>tt', ":'<,'>Gtrans<CR>", { desc = 'Dịch Google' })
+    -- Dịch chuẩn bằng AI
+    vim.keymap.set('v', '<leader>ta', ":'<,'>Atrans<CR>", { desc = 'Dịch Gemini AI' })
   end,
 }

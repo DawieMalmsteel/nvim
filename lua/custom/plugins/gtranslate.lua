8a242d7b0c565a632b08dea3ac2937d12a068214
dwcks <corclan19@gmail.com>
2025-12-29 15:18:57 +0700

    current

--- lua/custom/plugins/gtranslate.lua
+++ lua/custom/plugins/gtranslate.lua
@@ -5,11 +5,13 @@ return {
   config = function()
     require('gtranslate').setup {
       target_lang = 'vi',
+      gemini_model = 'gemini-2.0-flash',
     }
 
     -- Dịch nhanh bằng Google
-    vim.keymap.set('v', '<leader>tt', ":'<,'>Gtrans<CR>", { desc = 'Dịch Google' })
+    vim.keymap.set('v', '<leader>tt', ":'<,'>Gtrans<CR>", { desc = 'Google Translate' })
     -- Dịch chuẩn bằng AI
-    vim.keymap.set('v', '<leader>ta', ":'<,'>Atrans<CR>", { desc = 'Dịch Gemini AI' })
+    vim.keymap.set('v', '<leader>ta', ":'<,'>Atrans<CR>", { desc = 'Gemini AI Translate' })
+    vim.keymap.set('v', '<leader>te', ":'<,'>Etrans<CR>", { desc = 'Gemini Explain' })
   end,
 }

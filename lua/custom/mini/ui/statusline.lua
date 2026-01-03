3d103d5ddec0f012f76eed37ed2a8967e72db9ac
dwcks <corclan19@gmail.com>
2026-01-03 15:47:57 +0700

    update statusline for fit with new tabline

--- lua/custom/mini/ui/statusline.lua
+++ lua/custom/mini/ui/statusline.lua
@@ -73,90 +73,153 @@ local M = function()
     return '%#MiniStatuslineHarpoon#' .. table.concat(parts, ' ') .. '%#StatusLine#'
   end
 
-  -- 4. TABS LIST
-  local function section_tabs_list()
-    local buffers = vim.tbl_filter(function(b)
-      return vim.api.nvim_buf_is_valid(b.bufnr) and vim.bo[b.bufnr].buflisted
-    end, vim.fn.getbufinfo())
-    if #buffers == 0 then
-      return ''
+  -- 4. TABS LIST (DISABLED)
+  -- local function section_tabs_list()
+  --   local buffers = vim.tbl_filter(function(b)
+  --     return vim.api.nvim_buf_is_valid(b.bufnr) and vim.bo[b.bufnr].buflisted
+  --   end, vim.fn.getbufinfo())
+  --   if #buffers == 0 then
+  --     return ''
+  --   end
+
+  --   local cur_buf = vim.api.nvim_get_current_buf()
+  --   local max_view = 3
+  --   local cur_idx = 1
+  --   for i, b in ipairs(buffers) do
+  --     if b.bufnr == cur_buf then
+  --       cur_idx = i
+  --       break
+  --     end
+  --   end
+
+  --   local start_i = math.max(1, cur_idx - math.floor(max_view / 2))
+  --   local end_i = math.min(#buffers, start_i + max_view - 1)
+  --   if end_i - start_i + 1 < max_view and end_i == #buffers then
+  --     start_i = math.max(1, end_i - max_view + 1)
+  --   end
+
+  --   local parts = {}
+  --   for i = start_i, end_i do
+  --     local b = buffers[i]
+  --     local bid = b.bufnr
+  --     local path = b.name
+  --     local is_active = (bid == cur_buf)
+  --     local is_mod = vim.bo[bid].modified
+
+  --     local name = (path == '') and '[No Name]' or vim.fn.fnamemodify(path, ':t')
+
+  --     if is_active then
+  --       local max_active_len = 20
+  --       if #name > max_active_len then
+  --         name = '…' .. name:sub(-(max_active_len - 1))
+  --       end
+  --     else
+  --       name = vim.fn.fnamemodify(name, ':r')
+  --       if #name > 8 then
+  --         name = name:sub(1, 7) .. '…'
+  --       end
+  --     end
+
+  --     local icon, icon_hl_group = icons.get('file', path)
+  --     local icon_str = string.format('%%#%s#%s', icon_hl_group, icon)
+
+  --     local text_hl = 'MiniStatuslineInactive'
+  --     local status_icon = ''
+
+  --     local errs = #diagnostic.get(bid, { severity = diagnostic.severity.ERROR })
+  --     local warns = #diagnostic.get(bid, { severity = diagnostic.severity.WARN })
+
+  --     if is_active then
+  --       text_hl = 'MiniStatuslineFilenameActive'
+  --       if is_mod then
+  --         status_icon = ' ●'
+  --       end
+  --     else
+  --       if errs > 0 then
+  --         text_hl = 'MiniStatuslineFilenameError'
+  --       elseif warns > 0 then
+  --         text_hl = 'MiniStatuslineFilenameWarn'
+  --       elseif is_mod then
+  --         text_hl = 'MiniStatuslineFilenameMod'
+  --         status_icon = ' ●'
+  --       end
+  --     end
+
+  --     local item = string.format('%s %%#%s#%s%s', icon_str, text_hl, name, status_icon)
+  --     table.insert(parts, item)
+  --   end
+
+  --   if start_i > 1 then
+  --     table.insert(parts, 1, '%#MiniStatuslineFilenameInactive#')
+  --   end
+  --   if end_i < #buffers then
+  --     table.insert(parts, '%#MiniStatuslineFilenameInactive#')
+  --   end
+
+  --   return table.concat(parts, '%#MiniStatuslineFilenameInactive# ') .. '%#StatusLine#'
+  -- end
+
+  -- 4. SINGLE FILE PATH
+  local function section_file_path()
+    local path = vim.fn.expand '%:p'
+    if path == '' then
+      return '%#MiniStatuslineFilenameActive#[No Name]'
     end
 
-    local cur_buf = vim.api.nvim_get_current_buf()
-    local max_view = 3
-    local cur_idx = 1
-    for i, b in ipairs(buffers) do
-      if b.bufnr == cur_buf then
-        cur_idx = i
-        break
+    local is_oil = vim.bo.filetype == 'oil'
+    local is_active = true
+    local is_mod = vim.bo.modified
+    local text_hl = 'MiniStatuslineFilenameActive'
+    local status_icon = ''
+
+    local errs = #diagnostic.get(0, { severity = diagnostic.severity.ERROR })
+    local warns = #diagnostic.get(0, { severity = diagnostic.severity.WARN })
+
+    if errs > 0 then
+      text_hl = 'MiniStatuslineFilenameError'
+    elseif warns > 0 then
+      text_hl = 'MiniStatuslineFilenameWarn'
+    elseif is_mod then
+      text_hl = 'MiniStatuslineFilenameMod'
+      status_icon = ' ●'
+    end
+
+    if is_oil then
+      local dir_icon, dir_icon_hl = icons.get('file', path)
+      local dir_icon_str = string.format('%%#%s#%s', dir_icon_hl, dir_icon)
+      return string.format('%s %%#%s#%s%%#StatusLine#', dir_icon_str, text_hl, path)
+    end
+
+    local icon, icon_hl_group = icons.get('file', path)
+    local icon_str = string.format('%%#%s#%s', icon_hl_group, icon)
+
+    local rel_path = vim.fn.fnamemodify(path, ':~:.')
+    local parts = vim.split(rel_path, '/', { plain = true })
+    if #parts > 1 then
+      local filename = parts[#parts]
+      local dirs = { parts[1] }
+
+      for i = 2, #parts - 1 do
+        table.insert(dirs, parts[i]:sub(1, 1))
       end
-    end
 
-    local start_i = math.max(1, cur_idx - math.floor(max_view / 2))
-    local end_i = math.min(#buffers, start_i + max_view - 1)
-    if end_i - start_i + 1 < max_view and end_i == #buffers then
-      start_i = math.max(1, end_i - max_view + 1)
-    end
+      local ext = vim.fn.fnamemodify(filename, ':e')
+      local name_only = vim.fn.fnamemodify(filename, ':t:r')
+      local full_ext = #ext > 0 and ('.' .. ext) or ''
 
-    local parts = {}
-    for i = start_i, end_i do
-      local b = buffers[i]
-      local bid = b.bufnr
-      local path = b.name
-      local is_active = (bid == cur_buf)
-      local is_mod = vim.bo[bid].modified
-
-      local name = (path == '') and '[No Name]' or vim.fn.fnamemodify(path, ':t')
-
-      if is_active then
-        local max_active_len = 20
-        if #name > max_active_len then
-          name = '…' .. name:sub(-(max_active_len - 1))
-        end
-      else
-        name = vim.fn.fnamemodify(name, ':r')
-        if #name > 8 then
-          name = name:sub(1, 7) .. '…'
+      if #filename > 25 then
+        local max_name = 25 - #full_ext
+        if max_name > 3 then
+          name_only = '...' .. name_only:sub(-(max_name - 3))
+        else
+          name_only = name_only:sub(-25 + #full_ext)
         end
       end
 
-      local icon, icon_hl_group = icons.get('file', path)
-      local icon_str = string.format('%%#%s#%s', icon_hl_group, icon)
-
-      local text_hl = 'MiniStatuslineInactive'
-      local status_icon = ''
-
-      local errs = #diagnostic.get(bid, { severity = diagnostic.severity.ERROR })
-      local warns = #diagnostic.get(bid, { severity = diagnostic.severity.WARN })
-
-      if is_active then
-        text_hl = 'MiniStatuslineFilenameActive'
-        if is_mod then
-          status_icon = ' ●'
-        end
-      else
-        if errs > 0 then
-          text_hl = 'MiniStatuslineFilenameError'
-        elseif warns > 0 then
-          text_hl = 'MiniStatuslineFilenameWarn'
-        elseif is_mod then
-          text_hl = 'MiniStatuslineFilenameMod'
-          status_icon = ' ●'
-        end
-      end
-
-      local item = string.format('%s %%#%s#%s%s', icon_str, text_hl, name, status_icon)
-      table.insert(parts, item)
+      rel_path = table.concat(dirs, '/') .. '/' .. name_only .. full_ext
     end
 
-    if start_i > 1 then
-      table.insert(parts, 1, '%#MiniStatuslineFilenameInactive#')
-    end
-    if end_i < #buffers then
-      table.insert(parts, '%#MiniStatuslineFilenameInactive#')
-    end
-
-    return table.concat(parts, '%#MiniStatuslineFilenameInactive# ') .. '%#StatusLine#'
+    return string.format('%s %%#%s#%s%s%%#StatusLine#', icon_str, text_hl, rel_path, status_icon)
   end
 
   -- 5. CONFIG CHÍNH
@@ -176,7 +239,7 @@ local M = function()
         return statusline.combine_groups {
           { hl = mode_hl, strings = { mode } },
           { hl = 'MiniStatuslineDevinfo', strings = { git } },
-          { strings = { section_tabs_list() } },
+          { strings = { section_file_path() } },
           { strings = { section_diagnostics() } },
           { strings = { section_harpoon() } },
           '%<',

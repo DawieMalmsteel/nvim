61ad94e86def61148a49157143ca44a48ff49c0f
dwcks <corclan19@gmail.com>
2026-01-06 23:34:24 +0700

    update mini plugins

--- lua/custom/keymaps/zen.lua
+++ lua/custom/keymaps/zen.lua
@@ -1,9 +1,19 @@
 local map = vim.keymap.set
 
-map('n', '<leader>z', function()
-  local Snacks = require 'snacks'
-  Snacks.zen()
-end, { desc = 'Toggle Zen Mode' })
+-- map('n', '<leader>z', function()
+--   local Snacks = require 'snacks'
+--   Snacks.zen()
+-- end, { desc = 'Toggle Zen Mode' })
+
+map('n', '<leader>zz', function()
+  local Mini = require 'mini.misc'
+  Mini.zoom()
+end, { desc = 'Toggle Zoom Mode' })
+
+map('n', '<leader>zr', function()
+  local Mini = require 'mini.misc'
+  Mini.resize_window()
+end, { desc = 'Resize window' })
 
 map('n', '<leader>Z', function()
   local Snacks = require 'snacks'

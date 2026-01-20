622583840b521fe9e3ca3082ea6281aea2c7242b
dwcks <corclan19@gmail.com>
2026-01-20 15:31:01 +0700

    added night owl

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -9,8 +9,8 @@ return {
       require('tokyodark').setup(opts) -- calling setup is optional
       -- vim.cmd [[colorscheme tokyodark]]
       -- vim.cmd [[colorscheme tokyonight-night]]
-      -- vim.cmd [[colorscheme catppuccin]]
-      vim.cmd [[colorscheme night-owl]]
+      vim.cmd [[colorscheme catppuccin]]
+      -- vim.cmd [[colorscheme night-owl]]
     end,
   },
   'oxfist/night-owl.nvim',

f46aed08856b5654e81962f105964662e522da67
dwcks <corclan19@gmail.com>
2026-02-14 12:53:40 +0700

    cursortab unusable

--- lua/custom/plugins/cursortab.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'leonardcser/cursortab.nvim',
-  -- version = "*",  -- Use latest tagged version for more stability
-  build = 'cd server && go build',
-  config = function()
-    require('cursortab').setup()
-  end,
-}

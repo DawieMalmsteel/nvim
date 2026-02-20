7ee65dda6cca59a9707f48bdf1d04a179059a9cb
dwcks <corclan19@gmail.com>
2026-02-21 01:38:05 +0700

    changed snacks layout for picker

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -4,6 +4,30 @@ return {
     opts = {
       picker = {
         ui_select = true,
+        layout = {
+          select = {
+            layout = 'ivy',
+          },
+          layout = {
+            box = 'horizontal',
+            backdrop = false,
+            width = 0.8,
+            height = 0.9,
+            border = 'none',
+            {
+              box = 'vertical',
+              { win = 'input', height = 1, border = true, title = '{title} {live} {flags}', title_pos = 'center' },
+              { win = 'list', title = ' Results ', title_pos = 'center', border = true },
+            },
+            {
+              win = 'preview',
+              title = '{preview:Preview}',
+              width = 0.5,
+              border = true,
+              title_pos = 'center',
+            },
+          },
+        },
         previewers = {
           git = {
             native = true,

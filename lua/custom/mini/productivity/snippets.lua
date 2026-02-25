933f1853424ccb1216c765ae63848a23c24085b9
dwcks <corclan19@gmail.com>
2026-02-25 22:39:36 +0700

    Changed <C-i> to <C-a>

--- lua/custom/mini/productivity/snippets.lua
+++ lua/custom/mini/productivity/snippets.lua
@@ -23,7 +23,7 @@ local M = function()
     },
     mappings = {
       -- Expand snippet at cursor position. Created globally in Insert mode.
-      expand = '<C-i>',
+      expand = '<C-a>',
       -- Interact with default `expand.insert` session.
       -- Created for the duration of active session(s)
       jump_next = '<C-n>',

3080f069e874b048eb0243febacb122f36b40319
dwcks <corclan19@gmail.com>
2026-03-15 15:40:56 +0700

    added kanagawa

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -92,13 +92,23 @@ return {
     'Mofiqul/vscode.nvim',
   },
   {
+    'thesimonho/kanagawa-paper.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+
+    config = function(_, opts)
+      vim.cmd [[colorscheme kanagawa-paper]]
+    end,
+  },
+  {
     'navarasu/onedark.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('onedark').setup {
-        style = 'deep',
-      }
-      require('onedark').load()
-    end,
+    -- config = function()
+    --   require('onedark').setup {
+    --     style = 'deep',
+    --   }
+    --   require('onedark').load()
+    -- end,
   },
 }

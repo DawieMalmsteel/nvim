1386fdf928722471894e9e0012cf009943e5d5de
dwcks <corclan19@gmail.com>
2026-04-10 11:06:23 +0700

    refactor(fold): refactor ufo

--- lua/custom/plugins/ufo.lua
+++ lua/custom/plugins/ufo.lua
@@ -1,9 +1,6 @@
 return {
-  -- Configure nvim-ufo
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = 'kevinhwang91/promise-async',
-    event = 'BufReadPost', -- Load UFO after reading a buffer
-    opts = {},
-  },
+  'kevinhwang91/nvim-ufo',
+  dependencies = 'kevinhwang91/promise-async',
+  event = 'BufReadPost', -- Load UFO after reading a buffer
+  opts = {},
 }

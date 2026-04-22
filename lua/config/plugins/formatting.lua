040a26949909d2fe7ea38b3a7507335a1d80e7e9
dwcks <corclan19@gmail.com>
2026-04-22 17:03:17 +0700

    feat: remove the format on save

--- lua/config/plugins/formatting.lua
+++ lua/config/plugins/formatting.lua
@@ -14,13 +14,13 @@ return {
   },
   opts = {
     notify_on_error = false,
-    format_on_save = function(bufnr)
-      local disable = { c = true, cpp = true }
-      if disable[vim.bo[bufnr].filetype] then
-        return nil
-      end
-      return { timeout_ms = 500, lsp_format = 'fallback' }
-    end,
+    -- format_on_save = function(bufnr)
+    --   local disable = { c = true, cpp = true }
+    --   if disable[vim.bo[bufnr].filetype] then
+    --     return nil
+    --   end
+    --   return { timeout_ms = 500, lsp_format = 'fallback' }
+    -- end,
     formatters_by_ft = {
       lua = { 'stylua' },
     },

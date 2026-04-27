eac367a8655c1313344ac81989cac19ccba4be6d
dwcks <corclan19@gmail.com>
2026-04-27 17:40:28 +0700

    removed unused

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -16,23 +16,6 @@ return {
       vim.cmd 'colorscheme nightingale'
     end,
   },
-  -- 'sainnhe/everforest',
-  -- {
-  --   'e-ink-colorscheme/e-ink.nvim',
-  --   priority = 1000,
-  --   config = function()
-  --     require('e-ink').setup()
-  --     -- vim.opt.background = 'light'
-  --
-  --     -- choose light mode or dark mode
-  --     -- vim.opt.background = "dark"
-  --     -- vim.opt.background = "light"
-  --     --
-  --     -- or do
-  --     -- :set background=dark
-  --     -- :set background=light
-  --   end,
-  -- },
   {
     'wtfox/jellybeans.nvim',
     lazy = false,
@@ -43,12 +26,5 @@ return {
       italics = true,
       bold = true,
     }, -- Optional
-  },
-  {
-    'ankushbhagats/pastel.nvim',
-    lazy = false, -- disable lazy loading
-    priority = 1000, -- load immediately at startup
-    opts = {}, -- your configuration comes here
-    config = true, -- call setup function with provided opts
   },
 }

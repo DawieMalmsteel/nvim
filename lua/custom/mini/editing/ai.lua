acd457cbad80093d06d2973e49df21f6aee229c8
dwcks <corclan19@gmail.com>
2026-04-27 17:39:08 +0700

    bring back mini ai

--- lua/custom/mini/editing/ai.lua
+++ lua/custom/mini/editing/ai.lua
@@ -5,6 +5,15 @@ local M = function()
   --  - va)  - [V]isually select [A]round [)]paren
   --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
   --  - ci'  - [C]hange [I]nside [']quote
-  require('mini.ai').setup { n_lines = 500 }
+  require('mini.ai').setup {
+    n_lines = 500,
+    -- mappings = { around_next = 'an', inside_next = 'in', around_last = 'al', inside_last = 'il' },
+    mappings = {
+      around_next = '',
+      inside_next = '',
+      around_last = '',
+      inside_last = '',
+    },
+  }
 end
 return M

0ba1376c49fb7c99c8f6bbdf55acebb371ef0ede
--global <corclan19@gmail.com>
2026-06-26 11:11:17 +0700

    added diffs

---
+++ lua/custom/plugins/diffs.lua
@@ -0,0 +1,13 @@
+vim.g.diffs = {
+  integrations = {
+    fugitive = true,
+    neogit = true,
+    neojj = true,
+    gitsigns = true,
+  },
+}
+
+return {
+  'barrettruth/diffs.nvim',
+  'justinmk/guh.nvim',
+}

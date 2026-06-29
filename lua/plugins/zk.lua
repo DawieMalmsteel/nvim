5efa6ad5b9471e8012fcd0fb4293c82fa4d52cc2
--global <corclan19@gmail.com>
2026-06-29 16:42:42 +0700

    update zk

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -1,18 +1,5 @@
 return {
   'zk-org/zk-nvim',
   name = 'zk',
-  ft = 'markdown',
-  opts = {
-    picker = 'snacks_picker',
-    lsp = {
-      config = {
-        name = 'zk',
-        cmd = { 'zk', 'lsp' },
-        filetypes = { 'markdown' },
-      },
-      auto_attach = {
-        enabled = true,
-      },
-    },
-  },
+  opts = {},
 }

abc68e8ba56b311eea4ac6ef187814daebfd5f64
--global <corclan19@gmail.com>
2026-07-11 18:13:44 +0700

    bring back obsidian (bạn tao đó)

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,27 @@
+vim.opt_local.conceallevel = 2
+return {
+  'obsidian-nvim/obsidian.nvim',
+  version = '*', -- use latest release, remove to use latest commit
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, -- this will be removed in 4.0.0
+    workspaces = {
+      {
+        name = 'work',
+        path = '~/funthings/',
+      },
+    },
+    note_id_func = function(title)
+      if title == nil or vim.trim(title) == '' then
+        return tostring(os.time())
+      end
+
+      return vim.trim(title):gsub('[/\\:*?"<>|]', '-')
+    end,
+
+    ui = {
+      enable = false,
+    },
+  },
+}

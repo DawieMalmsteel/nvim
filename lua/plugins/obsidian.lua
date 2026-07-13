12b957f725f10254e65f8da67c8fee3f6f95afd9
--global <corclan19@gmail.com>
2026-07-13 22:26:24 +0700

    use snacks picker at default

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -5,6 +5,9 @@ return {
   ---@module 'obsidian'
   ---@type obsidian.config
   opts = {
+    picker = {
+      name = 'snacks.picker',
+    },
     legacy_commands = false, -- this will be removed in 4.0.0
     workspaces = {
       {

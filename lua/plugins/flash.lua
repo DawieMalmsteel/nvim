de1f20ab293dbbf226c3333aa8290d56a772dc77
--global <corclan19@gmail.com>
2026-07-27 21:24:46 +0700

    fixed flash nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,7 +1,12 @@
 return {
   'folke/flash.nvim',
-  vscode = true,
-  opts = {},
+  opts = {
+    modes = {
+      char = {
+        enabled = true,
+      },
+    },
+  },
   keys = {
     {
       'S',
@@ -18,6 +23,54 @@ return {
         require('flash').treesitter()
       end,
       desc = 'Flash Treesitter',
+    },
+    {
+      'f',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump {
+          search = { mode = 'search', max_length = 0 },
+          label = { after = { 0, 0 } },
+          pattern = '^',
+        }
+      end,
+      desc = 'Flash f',
+    },
+    {
+      'F',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump {
+          search = { mode = 'search', max_length = 0 },
+          label = { after = { 0, 0 } },
+          pattern = '^',
+        }
+      end,
+      desc = 'Flash F',
+    },
+    {
+      't',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump {
+          search = { mode = 'search', max_length = 0 },
+          label = { after = { 0, 0 } },
+          pattern = '.',
+        }
+      end,
+      desc = 'Flash t',
+    },
+    {
+      'T',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump {
+          search = { mode = 'search', max_length = 0 },
+          label = { after = { 0, 0 } },
+          pattern = '.',
+        }
+      end,
+      desc = 'Flash T',
     },
   },
 }

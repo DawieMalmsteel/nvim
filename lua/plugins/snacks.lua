03cc13ec0c49f9cdba9e86e602e940758c2a880d
--global <corclan19@gmail.com>
2026-08-17 01:13:11 +0700

    update snacsk

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -487,22 +487,22 @@ return {
       end,
       desc = 'Outgoing Calls',
     },
-    {
-      ']]',
-      function()
-        Snacks.words.jump(vim.v.count1)
-      end,
-      desc = 'Next Reference',
-      mode = { 'n', 't' },
-    },
-    {
-      '[[',
-      function()
-        Snacks.words.jump(-vim.v.count1)
-      end,
-      desc = 'Prev Reference',
-      mode = { 'n', 't' },
-    },
+    -- {
+    --   ']]',
+    --   function()
+    --     Snacks.words.jump(vim.v.count1)
+    --   end,
+    --   desc = 'Next Reference',
+    --   mode = { 'n', 't' },
+    -- },
+    -- {
+    --   '[[',
+    --   function()
+    --     Snacks.words.jump(-vim.v.count1)
+    --   end,
+    --   desc = 'Prev Reference',
+    --   mode = { 'n', 't' },
+    -- },
 
     -- buffers/ui/tools
     {
